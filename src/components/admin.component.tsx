ffee942a767875abdbe574861304ceb22b262bd8
Sergey Miranovich <belarus1552@outlook.com>
2022-08-11 14:41:18 +0300

    admin component added

---
+++ src/components/admin.component.tsx
@@ -0,0 +1,12 @@
+import { Component, PropsWithChildren } from "react";
+
+type Props = {};
+type State = {};
+class Admin extends Component<Props, State>{
+    render(){
+        return(
+            <h1>Hello</h1>
+        );
+    }
+}
+export default Admin;
