a03c053e55124fb932849a9327be2c648a8fff9c
Sergey Miranovich <belarus1552@outlook.com>
2022-08-26 15:49:31 +0300

    created: user service

--- src/components/admin.component.tsx
+++ src/components/admin.component.tsx
@@ -3,9 +3,34 @@ import { Component, PropsWithChildren } from "react";
 type Props = {};
 type State = {};
 class Admin extends Component<Props, State>{
+
+    headers: { key: string; label: string }[] = [
+        { key: "id", label: "ID" },
+        { key: "first_name", label: "First name" },
+        { key: "last_name", label: "Last name" },
+        { key: "email", label: "Email" },
+        { key: "gender", label: "Gender" },
+        { key: "ip_address", label: "IP address" },
+      ];
+
     render(){
+        data = 
         return(
-            <h1>Hello</h1>
+            <table>
+                <thead>
+                    <tr>
+                        {this.headers.map((row) => {
+                            return(
+                                <td key={row.key}>{row.label}{" "}</td>
+                            );
+                        })}
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {}
+                </tbody>
+            </table>
         );
     }
 }
