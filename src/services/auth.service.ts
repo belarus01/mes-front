785dc6b45b06ab8e0186f65b3d3194708b225bd1
Sergey Miranovich <belarus1552@outlook.com>
2022-09-05 11:39:33 +0300

    changed: token error

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -9,7 +9,7 @@ class AuthService{
                 password
             })
             .then(response => {
-                if(response.data.accessToken){
+                if(response.data.token){
                     localStorage.setItem("user", JSON.stringify(response.data))
                 }
                 return response.data;
